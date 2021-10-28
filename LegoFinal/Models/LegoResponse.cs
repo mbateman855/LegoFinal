a68ab5a2b77c2b9ef4e4ab35b6810ed3e5670d58
Morgan Bateman <mbateman855@gmail.com>
2021-10-28 19:01:11 -0400

    Changed the API endpoint for pasting JSON as classes

--- LegoFinal/Models/LegoResponse.cs
+++ LegoFinal/Models/LegoResponse.cs
@@ -16,22 +16,14 @@ namespace LegoFinal.Models
 
     public class Result
     {
-        public string part_num { get; set; }
+        public string set_num { get; set; }
         public string name { get; set; }
-        public int part_cat_id { get; set; }
-        public string part_url { get; set; }
-        public string part_img_url { get; set; }
-        public External_Ids external_ids { get; set; }
-        public string print_of { get; set; }
-    }
-
-    public class External_Ids
-    {
-        public string[] BrickLink { get; set; }
-        public string[] BrickOwl { get; set; }
-        public string[] Brickset { get; set; }
-        public string[] LEGO { get; set; }
-        public string[] LDraw { get; set; }
+        public int year { get; set; }
+        public int theme_id { get; set; }
+        public int num_parts { get; set; }
+        public string set_img_url { get; set; }
+        public string set_url { get; set; }
+        public DateTime last_modified_dt { get; set; }
     }
 
 }
