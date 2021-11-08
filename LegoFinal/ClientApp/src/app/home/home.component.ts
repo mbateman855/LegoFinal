15dcb4880a43cbb20937180da3b63fbc8e22e995
Alesia - Imali SuMaku Kute <AlesiaLRuffin@gmail.com>
2021-11-07 21:15:07 -0500

    api appears on home page. it's ugly but it works

--- LegoFinal/ClientApp/src/app/home/home.component.ts
+++ LegoFinal/ClientApp/src/app/home/home.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
   setName!: string;
   result: string;
 
-  searchLegoSets(userInput: string) {
+  searchLegoResponse(userInput: string) {
     //this is where you do the filtering
     //this.result = sets.filter( x= > x == userInput)
-    console.log("falcon");
+    console.log("userInput");
   }
 
 }
+
+
+
+//you add below for testing on sunday 11-7-21 @ 1753 ALR';
