024e9ab88a14d117e96ac5f42aadd3200cb36cdb
Morgan Bateman <mbateman855@gmail.com>
2021-11-11 21:23:44 -0500

    added partial functionality for parts list display

--- LegoFinal/ClientApp/src/app/set-details/set-details.component.ts
+++ LegoFinal/ClientApp/src/app/set-details/set-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Results } from '../Models/LegoResponse';
+import { PartsResponse } from '../Models/PartsResponse';
+import { PartsService } from '../parts.service';
 import { TestService } from '../test.service';
 
 @Component({
@@ -11,17 +13,19 @@ import { TestService } from '../test.service';
 export class SetDetailsComponent implements OnInit {
 
   set!: Results;
+  parts!: PartsResponse;
   baseUrl: string;
 
-  constructor(private testService: TestService, private route: ActivatedRoute, @Inject('BASE_URL') baseUrl: string)
+  constructor(private testService: TestService, private partsService: PartsService, private route: ActivatedRoute, @Inject('BASE_URL') baseUrl: string)
   {
     this.baseUrl = baseUrl;
   }
 
-  ngOnInit() {
-    this.route.paramMap      .subscribe(params => {        let set_num = params.get('set_num');        console.log(set_num);        this.testService.getSetDetails(set_num, this.baseUrl).subscribe(x =>        {
-          this.set = x;          console.log(this.set);        }        );      });
 
+  ngOnInit() {
+    this.route.paramMap      .subscribe(params => {        let set_num = params.get('set_num');        console.log(set_num);        this.testService.getSetDetails(set_num, this.baseUrl).subscribe(x => {
+          this.set = x;          console.log(this.set);        });        this.partsService.getPartResults(set_num, this.baseUrl).subscribe(x => {          this.parts = x;          console.log(this.parts);
+        });      });
   }
 
 }
